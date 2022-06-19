50f3bd47dcfe801fb8b8386e153692d244f12b7d
ashutosh-saumitra <ashutoshsaumitra4u@gmail.com>
2022-06-19 12:04:48 +0530

    Exception2

--- src/ExceptionHandling/ThrowThrowsKeyword.java
+++ src/ExceptionHandling/ThrowThrowsKeyword.java
@@ -5,32 +5,22 @@ class RansomWare{
 		return a/b;
 	}
 	void action() throws Exception{
-		divide(2, 0);
-		try{
-			
-		}
-		catch(Exception g){
-			
-		}
+		System.out.println(divide(2, 0));
+		
 	}
 }
 public class ThrowThrowsKeyword {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		RansomWare ransomWare =new RansomWare();
-		try{
 			ransomWare.action();
-		}
-		catch(ArithmeticException | NullPointerException e){
-			e.printStackTrace();
-		}
-		catch(Exception aen){
-			aen.printStackTrace();
-		}
 		/* Throws:
 		 * -> After method name
 		 * -> As an exception is found, it will throw the exception, and the control will go to the point from where the method was called.
 		 * -> After the exception is found the rest of the code will not be executed.
 		 * -> When we do not know that we may/may not have an exception in our program.
+		 * */
+		/* Throw:
+		 * 
 		 * */
 	}
 
