a8f9827448300a0ad8d714ff403f38680517ec17
Sachin Oraon <sachinoraon55@gmail.com>
2022-06-27 23:36:16 +0530

    Update ThrowThrowsKeyword.java

--- src/ExceptionHandling/ThrowThrowsKeyword.java
+++ src/ExceptionHandling/ThrowThrowsKeyword.java
@@ -1,5 +1,7 @@
 package ExceptionHandling;
 
+import java.io.FileNotFoundException;
+
 class RansomWare{
 	int divide(int a, int b) throws ArithmeticException{
 		return a/b;
@@ -35,15 +37,27 @@ class RansomWare{
 			return a*a*a;
 		}
 	}
+	
+	protected void printClassName(){
+		System.out.println(this.getClass().getSimpleName());
+	}
 }
+
+class ChildClass extends RansomWare {
+	@Override
+	public void printClassName() throws FileNotFoundException /* error here because it's checked exception */{
+		System.out.println(this.getClass().getSimpleName());
+	}
+}
+
 public class ThrowThrowsKeyword {
 	public static void main(String[] args) {
 		RansomWare ransomWare =new RansomWare();
 			ransomWare.action();
 		/* Throws:
-		 * -> After method name
-		 * -> Only for checked exceptions.
-		 * -> As an exception is found, it will throw the exception, and the control will go to the point from where the method was called.
+		 * -> After method name i.e. used in the signature of method to indicate that this method might throw one of the listed type exceptions
+		 * -> The caller to these methods has to handle the exception using a try-catch block
+		 * -> When an exception is found, it will throw the exception, and the control will go to the point from where the method was called.
 		 * -> After the exception is found the rest of the code will not be executed.
 		 * -> When we do not know that we may/may not have an exception in our program.
 		 * -> The throws keyword is used for exception handling without try & catch block.
@@ -54,9 +68,13 @@ public class ThrowThrowsKeyword {
 		 * */
 			
 		/* Throw:
-		 * -> It is used to throw an exception explicitly in the code, inside the function or the block of code.
-		 * -> Throw is used within the method.
+		 * -> It is used to throw an exception(checked/unchecked) explicitly in the code, inside the function or the block of code.
+		 * -> Throw is used within the method and mainly used to throw custom exceptions.
 		 * -> We are allowed to throw only one exception at a time i.e. we cannot throw multiple exceptions.
+		 * */
+		
+		/* If the superclass method does not declare an exception, then subclass overridden method cannot declare the checked exception,
+		 * but it can declare unchecked exception
 		 * */
 	}
 
